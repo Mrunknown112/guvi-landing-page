8cfcb11ec799761478fb22f5bd6330ff57d6acd6
Vignesh A <129264808+Mrunknown112@users.noreply.github.com>
2026-07-07 15:15:54 +0530

    Add files via upload

---
+++ src/imports/Group2147230284/index.tsx
@@ -0,0 +1,322 @@
+import svgPaths from "./svg-rytvo4lz5a";
+import imgImage6345169 from "./f4cfa383ac619fa2102745b4fe9801c3fea23f44.png";
+import imgImage6345172 from "./03a52cca47c063f3fdc28705e0e38d5c922755c6.png";
+import imgImage6345173 from "./c3fc0f51e0e08ab6ce24c0cd33232e200f1a808a.png";
+import imgScreenshot20250906At124257Pm2 from "./9b673e416db24bbaa638aa92f0716f6412cdfd83.png";
+import { imgImage6345168 } from "./svg-3wa11";
+
+function MaskGroup() {
+  return (
+    <div className="absolute contents left-0 top-0" data-name="Mask group">
+      <div className="absolute h-[392.976px] left-[-18.5px] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[18.5px_8.828px] mask-size-[180px_254px] top-[-8.83px] w-[261.919px]" style={{ maskImage: `url("${imgImage6345168}")` }} data-name="image 6345168">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage6345169} />
+      </div>
+      <div className="absolute flex items-center justify-center left-[-4px] size-[355px] top-[-16px]">
+        <div className="-scale-y-100 flex-none rotate-180">
+          <div className="mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[4px_16px] mask-size-[180px_254px] relative size-[355px]" style={{ maskImage: `url("${imgImage6345168}")` }} data-name="image 6345172">
+            <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage6345172} />
+          </div>
+        </div>
+      </div>
+      <div className="-translate-x-1/2 -translate-y-1/2 absolute backdrop-blur-[2.777px] bg-gradient-to-b from-[77.049%] from-[rgba(0,0,0,0)] h-[254px] left-[calc(50%+0.25px)] mask-alpha mask-intersect mask-no-clip mask-no-repeat mask-position-[1.75px_57px] mask-size-[180px_254px] rounded-[34.715px] to-[97.131%] to-[rgba(0,0,0,0.65)] top-[calc(50%+0.5px)] w-[180px]" style={{ maskImage: `url("${imgImage6345168}")` }} />
+    </div>
+  );
+}
+
+function Frame() {
+  return (
+    <button className="block cursor-pointer h-[139px] overflow-clip relative rounded-[32px] shrink-0 w-full">
+      <MaskGroup />
+    </button>
+  );
+}
+
+function Frame4() {
+  return (
+    <div className="h-[198px] relative rounded-[34px] shrink-0 w-[176px]">
+      <div className="overflow-clip relative rounded-[inherit] size-full">
+        <div className="-translate-x-1/2 absolute flex h-[269px] items-center justify-center left-[calc(50%+20.5px)] top-[-25px] w-[403px]">
+          <div className="-scale-y-100 flex-none rotate-180">
+            <div className="h-[269px] relative w-[403px]" data-name="image 6345173">
+              <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgImage6345173} />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div aria-hidden className="absolute border-[#6729ff] border-[1.75px] border-solid inset-[-1.75px] pointer-events-none rounded-[35.75px]" />
+    </div>
+  );
+}
+
+function Frame5() {
+  return (
+    <button className="block cursor-pointer h-[138px] overflow-clip relative rounded-[34px] shrink-0 w-[176px]">
+      <div className="-translate-y-1/2 absolute aspect-[1346/958] left-[-9.66%] right-[-10.23%] top-1/2" data-name="Screenshot 2025-09-06 at 12.42.57 PM 2">
+        <img alt="" className="absolute inset-0 max-w-none object-cover pointer-events-none size-full" src={imgScreenshot20250906At124257Pm2} />
+      </div>
+    </button>
+  );
+}
+
+function Frame3() {
+  return (
+    <div className="absolute content-stretch flex flex-col gap-[14px] h-[509px] items-start left-0 overflow-clip px-[2px] top-0 w-[180px]">
+      <Frame />
+      <Frame4 />
+      <Frame5 />
+    </div>
+  );
+}
+
+function Group1() {
+  return (
+    <div className="absolute contents left-0 top-0">
+      <Frame3 />
+    </div>
+  );
+}
+
+function Group4() {
+  return (
+    <div className="absolute h-[48px] left-[204.25px] top-[239px] w-[33.44px]">
+      <div className="absolute inset-[0_0_0_-6.66%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 35.6663 48">
+          <g id="Group 2147236076">
+            <path d={svgPaths.p337f04f0} fill="var(--fill-0, #F9F6FF)" id="Polygon 2" stroke="var(--stroke-0, #6729FF)" strokeWidth="1.5" />
+            <path d={svgPaths.p34f247f0} fill="var(--fill-0, #F9F6FF)" id="Rectangle 1430109096" />
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Frame8() {
+  return (
+    <div className="[word-break:break-word] absolute h-[296px] left-[2.71px] not-italic top-[-27px] w-[893px]">
+      <p className="absolute font-['Jones*:Book',sans-serif] h-[104.565px] leading-[1.3] left-0 text-[#f0eaff] text-[222.201px] top-[-32px] w-[85.893px]">“</p>
+      <p className="absolute font-['Wanted_Sans:SemiBold',sans-serif] leading-[1.35] left-0 text-[#161616] text-[24px] top-[94px] w-[778px]">{`“I completed GUVI's Full Stack Development (MERN) Program, and their teaching with query resolution was excellent.”`}</p>
+      <p className="absolute font-['Wanted_Sans:Regular',sans-serif] leading-[1.4] left-0 text-[#161616] text-[20px] top-[178px] w-[768px]">{`I completed GUVI's Full Stack Development (MERN) Program, and their teaching with query resolution was excellent. My mentors used to explain concepts in Tamil in really clear-cut ways. Placement support along with mock interviews and job.`}</p>
+    </div>
+  );
+}
+
+function Icon() {
+  return (
+    <div className="absolute left-0 size-[22.936px] top-0" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 22.9364 22.9364">
+        <g id="Icon">
+          <path d={svgPaths.p2c0fb600} fill="var(--fill-0, #FDC700)" id="Vector" stroke="var(--stroke-0, #FDC700)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.91137" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button() {
+  return (
+    <div className="absolute left-0 rounded-[2.294px] size-[22.936px] top-0" data-name="Button">
+      <Icon />
+    </div>
+  );
+}
+
+function Icon1() {
+  return (
+    <div className="absolute left-0 size-[22.936px] top-0" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 22.9364 22.9364">
+        <g id="Icon">
+          <path d={svgPaths.p24577600} fill="var(--fill-0, #FDC700)" id="Vector" stroke="var(--stroke-0, #FDC700)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.91137" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button1() {
+  return (
+    <div className="absolute left-[25.23px] rounded-[2.294px] size-[22.936px] top-0" data-name="Button">
+      <Icon1 />
+    </div>
+  );
+}
+
+function Icon2() {
+  return (
+    <div className="absolute left-0 size-[22.936px] top-0" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 22.9364 22.9364">
+        <g id="Icon">
+          <path d={svgPaths.p1c18a200} fill="var(--fill-0, #FDC700)" id="Vector" stroke="var(--stroke-0, #FDC700)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.91137" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button2() {
+  return (
+    <div className="absolute left-[50.46px] rounded-[2.294px] size-[22.936px] top-0" data-name="Button">
+      <Icon2 />
+    </div>
+  );
+}
+
+function Icon3() {
+  return (
+    <div className="absolute left-0 size-[22.936px] top-0" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 22.9364 22.9364">
+        <g id="Icon">
+          <path d={svgPaths.pfefec80} fill="var(--fill-0, #FDC700)" id="Vector" stroke="var(--stroke-0, #FDC700)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.91137" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button3() {
+  return (
+    <div className="absolute left-[75.69px] rounded-[2.294px] size-[22.936px] top-0" data-name="Button">
+      <Icon3 />
+    </div>
+  );
+}
+
+function Icon4() {
+  return (
+    <div className="absolute left-0 size-[22.936px] top-0" data-name="Icon">
+      <svg className="absolute block inset-0 size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 22.9364 22.9364">
+        <g id="Icon">
+          <path d={svgPaths.p377d2900} id="Vector" stroke="var(--stroke-0, #D1D5DC)" strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.91137" />
+        </g>
+      </svg>
+    </div>
+  );
+}
+
+function Button4() {
+  return (
+    <div className="absolute left-[100.92px] rounded-[2.294px] size-[22.936px] top-0" data-name="Button">
+      <Icon4 />
+    </div>
+  );
+}
+
+function StarRating() {
+  return (
+    <div className="absolute h-[22.936px] left-[-0.29px] top-[326px] w-[123.856px]" data-name="StarRating">
+      <Button />
+      <Button1 />
+      <Button2 />
+      <Button3 />
+      <Button4 />
+    </div>
+  );
+}
+
+function Group7() {
+  return (
+    <div className="absolute contents left-[-0.29px] top-[326px]">
+      <p className="[word-break:break-word] absolute font-['Wanted_Sans:SemiBold',sans-serif] leading-[1.3] left-[-0.29px] not-italic text-[#161616] text-[24px] top-[361px] whitespace-nowrap">Kamal V</p>
+      <StarRating />
+    </div>
+  );
+}
+
+function Group8() {
+  return (
+    <div className="absolute contents left-[-0.29px] top-[323px]">
+      <Group7 />
+      <p className="[word-break:break-word] absolute font-['Wanted_Sans:Regular',sans-serif] leading-[1.4] left-[131.56px] not-italic text-[#67737e] text-[20px] top-[323px] whitespace-nowrap">(4/5)</p>
+    </div>
+  );
+}
+
+function Group5() {
+  return (
+    <div className="absolute h-[27px] left-[93px] top-[0.5px] w-[61.125px]">
+      <div className="absolute inset-[0_-0.82%]">
+        <svg className="block size-full" fill="none" preserveAspectRatio="none" viewBox="0 0 62.125 27">
+          <g id="Group 2147236078">
+            <path d={svgPaths.pf7c4980} fill="var(--stroke-0, #44474B)" id="Arrow 1" />
+            <path d="M0.5 14.5635H18.5" id="Arrow 2" stroke="var(--stroke-0, #44474B)" strokeLinecap="round" />
+            <g id="Group 2147228166">
+              <path clipRule="evenodd" d={svgPaths.p2c0a3300} fill="var(--fill-0, #0AD652)" fillRule="evenodd" id="Vector" />
+              <path clipRule="evenodd" d={svgPaths.p3424fb30} fill="var(--fill-0, white)" fillRule="evenodd" id="Vector_2" />
+            </g>
+          </g>
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+function Frame7() {
+  return (
+    <div className="absolute h-[28px] left-0 top-0 w-[355.125px]">
+      <p className="[word-break:break-word] absolute font-['Wanted_Sans:Regular',sans-serif] leading-[1.4] left-0 not-italic text-[#141415] text-[20px] top-0 whitespace-nowrap">Fresher</p>
+      <Group5 />
+      <p className="[word-break:break-word] absolute font-['Wanted_Sans:Regular',sans-serif] leading-[1.4] left-[178.13px] not-italic text-[#141415] text-[20px] top-0 whitespace-nowrap">Node JS Developer</p>
+    </div>
+  );
+}
+
+function Frame1() {
+  return (
+    <div className="absolute h-[28px] left-[24px] top-[8px] w-[355.125px]">
+      <Frame7 />
+    </div>
+  );
+}
+
+function Frame2() {
+  return (
+    <div className="absolute bg-[#f0eaff] h-[44px] left-[-0.29px] rounded-[30px] top-[269px] w-[403.125px]">
+      <Frame1 />
+    </div>
+  );
+}
+
+function Frame6() {
+  return (
+    <div className="absolute h-[447.936px] left-[270.29px] top-[51px] w-[853.186px]">
+      <Frame8 />
+      <Group8 />
+      <Frame2 />
+    </div>
+  );
+}
+
+function Group() {
+  return (
+    <div className="absolute contents left-[270.29px] top-[51px]">
+      <Frame6 />
+    </div>
+  );
+}
+
+function Group6() {
+  return (
+    <div className="absolute contents left-[270.29px] top-[51px]">
+      <Group />
+    </div>
+  );
+}
+
+function Group3() {
+  return (
+    <div className="absolute contents left-[204.25px] top-0">
+      <div className="absolute bg-[#f9f6ff] border-[#6729ff] border-[1.5px] border-solid inset-[0_0_0_18.43%] rounded-[36px] shadow-[0px_8px_15px_0px_rgba(103,41,255,0.1)]" />
+      <Group4 />
+      <Group6 />
+    </div>
+  );
+}
+
+export default function Group2() {
+  return (
+    <div className="relative size-full">
+      <Group1 />
+      <Group3 />
+    </div>
+  );
+}
